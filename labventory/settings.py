9b513d4e8799365138d2908f141b589974966be6
naphiale <nafia.levana19@gmail.com>
2024-11-19 23:05:37 +0700

    tugas 9

--- labventory/settings.py
+++ labventory/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-y@l^@o8cr9ddijk*ded_!u=b)q#gpvbmh@s2macc623ng*ss2a
 PRODUCTION = os.getenv("PRODUCTION", False)
 DEBUG = not PRODUCTION
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "nafia-levana-labventory.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "nafia-levana-labventory.pbp.cs.ui.ac.id", "10.0.2.2"]
 
 # Application defi
 
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'authentication',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'labventory.urls'
@@ -130,3 +133,12 @@ else:
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://nafia-levana/labventory.pbp.cs.ui.ac.id/", "https://nafia-levana/labventory.pbp.cs.ui.ac.id/"]
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
